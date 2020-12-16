5efa1fa6d34641e4c4eafba4e0bc0e43b42c065f
Lizaveta Yankina <lizavetayankina@gmail.com>
2020-12-16 15:02:55 +0300

    js

--- assepts/javascript/script.js
+++ assepts/javascript/script.js
@@ -1,10 +1,10 @@
-
+//navagation menu 
 const menu = document.querySelectorAll('a[href^="#"]')
-
 for(let nav of menu) {
   nav.addEventListener("click", function(e) {
     e.preventDefault() 
-   const about, focus, portfolio, team, work_step, price, Testimonials = anchor.getAttribute('href');
+   const about, focus, portfolio, team, work_step, 
+   price, Testimonials = anchor.getAttribute('href');
     document.querySelector(about + focus + portfolio + team + work_step + price + Testimonials).scrollIntoView({
       behavior: "smooth",
       block: "start"
@@ -12,13 +12,16 @@ for(let nav of menu) {
   })
 }
 
+//no work. play or pause video
 const videoElement = document.querySelector('.myPlayer');
 const btnVideo = document.querySelector('.vid_icon');
 
 btnVideo.addEventListener('click', () => {
-  //videoElement.play();
-  alert('dvgds')
-  
+videoElement.play(); 
 })
 
+const btnForm = document.querySelector('.btnSubmit');
 
+btnForm.addEventListener('click', function () {
+alert('dfdfv');
+})
