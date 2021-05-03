a32f7b180ca0b606fe2cb3fa86014036e085718b
Lizaveta Yankina <lizavetayankina@gmail.com>
2021-05-03 14:13:05 +0300

    try work with form

--- assepts/javascript/script.js
+++ assepts/javascript/script.js
@@ -11,6 +11,8 @@ for(let nav of menu) {
   })
 }
 
+//play video
+
 const videoElement = document.querySelector('.myPlayer');
 const btnVideo = document.querySelector('.vid_icon');
 const videoTitile = document.querySelector('.video_title');
@@ -21,11 +23,36 @@ btnVideo.addEventListener('click', () => {
     videoTitile.innerHTML = "";
     videoUnderTitile.innerHTML = "";
     btnVideo.innerHTML = '';
-  } 
+  } else  {
+    videoTitile.innerHTML = "Explore Our Awesomeness";
+
+    videoUnderTitile.innerHTML = "Click to play the video";
+  }
 })
 
+
+//work with Forma
 const btnForm = document.querySelector('.btnSubmit');
 
-btnForm.addEventListener('click', function () {
-alert('dfdfv');
+btnForm.addEventListener('click',  () => {
+valid();
 })
+
+function getValue (){
+const name = document.querySelector('input[type= "name"]').value;
+const email = document.querySelector('input[type= "email"]').value;
+const subject = document.querySelector('input[type="Subject"]').value;
+const project = document.querySelector('input[type="Project"]').value;
+const textarea = document.querySelector('textarea').value;
+const userInfo = {name, email, subject,project, textarea}
+console.log(userInfo);
+}
+
+function valid() {
+const inp = document.querySelector('input');
+  if (inp.value !== "") { inp.classList.add('error');
+} else {
+  inp.classList.remove('error');
+  getValue();
+}
+}
